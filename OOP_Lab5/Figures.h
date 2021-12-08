f2df2c6480b2a48f6ee5a767b8c7000a293ce891
Nezarik-Intmax <nezarikintmax@gmail.com>
2021-12-08 20:37:57 +0500

    60%

--- OOP_Lab5/Figures.h
+++ OOP_Lab5/Figures.h
@@ -8,6 +8,9 @@ public:
 	Figure(Figure* a){
 		std::cout << "Figure(copy) constructor\n";
 	}
+	Figure(Figure& a){
+		std::cout << "Figure(copy) constructor\n";
+	}
 	~Figure(){
 		std::cout << "Figure destructor\n";
 	}
@@ -20,9 +23,17 @@ public:
 	}
 };
 class Line: public Figure{
+private:
+	int len;
 public:
-	Line(){
+	Line():len(0){
 		std::cout << "Line constructor\n";
+	}
+	Line(int len):len(len){
+		std::cout << "Line constructor\n";
+	}
+	Line(Line* a):len(a->getLength()){
+		std::cout << "Line(copy) constructor\n";
 	}
 	~Line(){
 		std::cout << "Line destructor\n";
@@ -33,5 +44,11 @@ public:
 	void draw(){
 		std::cout << "Line";
 		endDraw();
+	}
+	void length(){
+		std::cout << "Length: " << len << "\n";
+	}
+	int getLength(){
+		return len;
 	}
 };
--- OOP_Lab5/OOP_Lab5.cpp
+++ OOP_Lab5/OOP_Lab5.cpp
@@ -1,9 +1,17 @@
 ﻿#include <iostream>
 #include <memory>
-//#include "Figures.h";
-#include "FiguresVirtual.h";
+#include "Figures.h";
+//#include "FiguresVirtual.h";
 
-
+Figure ReturnFigure(Figure a){			// Сздается а
+	return a;							// Создается копия а, а - удаляется.
+}
+Figure* ReturnFigure(Figure* a){		
+	return a;							// переменная указатель "а" удаляется, но выделенная память остается и передается из функции
+}
+std::unique_ptr<Figure> ReturnFigureUnique(std::unique_ptr<Figure> a){
+	return a;							// а передается из функции если результат функции не записывается в переменную, то память освобождается
+}
 
 int main(){
 	Figure* a = new Figure();
@@ -15,12 +23,38 @@ int main(){
 	c->draw();
 	std::cout << "\n";
 
+	/*
 	Line* d = dynamic_cast<Line*>(b);
 	d->draw();
 	d->length();
+	//*/
 
 	delete(a);
 	delete(b);
 	delete(c);
+	std::cout << "\n\n\n";
 
+	//*
+	{
+		std::unique_ptr<Figure> e = std::make_unique<Figure>();
+		std::unique_ptr<Figure> f = std::move(e);					// f = e, e = nullptr;
+	}																// delete(f);
+	//*/
+	//*
+	{
+		std::unique_ptr<Figure> g = std::make_unique<Figure>();
+		std::unique_ptr<Figure> h = ReturnFigureUnique(std::move(g));	// h = g, g = nullptr;
+		ReturnFigureUnique(std::move(h));								// delete(h)
+	}
+	//*/
+	//*
+	{
+		std::shared_ptr<Line> i(new Line(10));	
+		{
+			std::shared_ptr<Line> j(i);			//i = j
+			j->length();						// 10
+		}
+		i->length();							// 10
+	}											// delete()
+	//*/
 }
